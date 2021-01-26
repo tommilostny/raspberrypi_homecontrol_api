d17391d9f00dbdc888290320b2b8bb55499fdae0
tommilostny <tommilostny@live.com>
2021-01-26 15:33:02 +0000

    Temperature threshold play with LEDs

--- heater_control.py
+++ heater_control.py
@@ -1,10 +1,15 @@
 import Adafruit_DHT
+from gpiozero import LED
 from time import sleep
 
 DHT_SENSOR = Adafruit_DHT.DHT11
 DHT_PIN = 4
-
 CELSIUS = u"\N{DEGREE SIGN}C"
+
+badtemp_led = LED(18)
+oktemp_led = LED(17)
+
+TEMPERATURE_THRESHOLD = 25
 
 def are_values_real(humidity, temperature):
    return(humidity is not None and temperature is not None and humidity < 100.0)
@@ -16,8 +21,13 @@ try:
         if are_values_real(humidity, temperature):
             print("Humidity: ", humidity, "%")
             print("Temperature: ", temperature, CELSIUS)
-            break
-        #sleep(1)
+            if temperature < TEMPERATURE_THRESHOLD:
+                oktemp_led.on()
+                badtemp_led.off()
+            else:
+                oktemp_led.off()
+                badtemp_led.on()
+        sleep(2)
 
 except KeyboardInterrupt:
     print("Cleaning up!")
