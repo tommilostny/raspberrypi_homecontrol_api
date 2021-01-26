fc484428a4181a295ec7e84dca2f8e28e544cf14
tommilostny <tommilostny@live.com>
2021-01-26 16:01:03 +0000

    Better one line print, ...

--- heater_control.py
+++ heater_control.py
@@ -14,20 +14,17 @@ TEMPERATURE_THRESHOLD = 25
 def are_values_real(humidity, temperature):
    return(humidity is not None and temperature is not None and humidity < 100.0)
 
-try:
-    while True:
-        humidity, temperature = Adafruit_DHT.read(DHT_SENSOR, DHT_PIN)
+while True:
+    humidity, temperature = Adafruit_DHT.read(DHT_SENSOR, DHT_PIN)
 
-        if are_values_real(humidity, temperature):
-            print("Humidity: ", humidity, "%")
-            print("Temperature: ", temperature, CELSIUS)
-            if temperature < TEMPERATURE_THRESHOLD:
-                oktemp_led.on()
-                badtemp_led.off()
-            else:
-                oktemp_led.off()
-                badtemp_led.on()
-        sleep(2)
+    if are_values_real(humidity, temperature):
+        print("Humidity: ", humidity, "%\tTemperature: ", temperature, CELSIUS, end="\r")
 
-except KeyboardInterrupt:
-    print("Cleaning up!")
+        if temperature < TEMPERATURE_THRESHOLD:
+            oktemp_led.on()
+            badtemp_led.off()
+        else:
+            oktemp_led.off()
+            badtemp_led.on()
+
+    sleep(1)
