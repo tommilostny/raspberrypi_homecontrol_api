0834e9ed9c0cf8e85ed12ad7aac2f7129ea95150
Tom Milostný <tommilostny@live.com>
2021-01-26 15:08:25 +0000

    Update to base program to display hum, temp better

--- heater_control.py
+++ heater_control.py
@@ -4,7 +4,7 @@ from time import sleep
 DHT_SENSOR = Adafruit_DHT.DHT11
 DHT_PIN = 4
 
-CELSIUS = chr(223) + "C"
+CELSIUS = u"\N{DEGREE SIGN}C"
 
 def are_values_real(humidity, temperature):
    return(humidity is not None and temperature is not None and humidity < 100.0)
@@ -14,10 +14,10 @@ try:
         humidity, temperature = Adafruit_DHT.read(DHT_SENSOR, DHT_PIN)
 
         if are_values_real(humidity, temperature):
-            print(humidity)
-            print(temperature)
-            print('\n')
-        sleep(2)
+            print("Humidity: ", humidity, "%")
+            print("Temperature: ", temperature, CELSIUS)
+            break
+        #sleep(1)
 
 except KeyboardInterrupt:
     print("Cleaning up!")
