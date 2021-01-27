168cdb19565364984ec9307907e70689c3a0c960
tommilostny <tommilostny@live.com>
2021-01-27 19:41:30 +0000

    Added Yeelight control endpoint

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from flask_restful import Api
 from testfiles import helloworld as hw
 from testfiles import video as v
 import led_control as lc
+import yeelight_control as yc
 
 app = Flask(__name__)
 api = Api(app)
@@ -13,6 +14,10 @@ api = Api(app)
 api.add_resource(hw.HelloWorld, "/helloworld/<string:name>")
 api.add_resource(v.Video, "/video/<int:video_id>")
 api.add_resource(lc.LED_Control, "/led/<string:status>")
+api.add_resource(yc.YeelightPower, "/yeelight/power/<string:status>")
+api.add_resource(yc.YeelightBrightness, "/yeelight/brightness/<int:brightness>")
+api.add_resource(yc.YeelightColor, "/yeelight/color/<int:r>/<int:g>/<int:b>")
+api.add_resource(yc.YeelightStatus, "/yeelight")
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0")
