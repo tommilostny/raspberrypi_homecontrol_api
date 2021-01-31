cd5b6f53e738da13f4347eaa2add8481c3964415
tommilostny <tommilostny@live.com>
2021-01-31 09:42:33 +0000

    Removed unused test tutorial stuff

--- main.py
+++ main.py
@@ -4,17 +4,12 @@ from flask_restful import Api
 from flask_cors import CORS
 
 #Local modules imports
-from testfiles import helloworld as hw
-from testfiles import video as v
 import led_control as lc
 import yeelight_control as yc
 
 app = Flask(__name__)
 api = Api(app)
 cors = CORS(app, resources={r"/*": {"origins": "*"}})
-
-api.add_resource(hw.HelloWorld, "/helloworld/<string:name>")
-api.add_resource(v.Video, "/video/<int:video_id>")
 
 api.add_resource(lc.LED_Control, "/led/<int:led_num>/<string:status>")
 api.add_resource(lc.LED_Status, "/led")
--- testfiles/helloworld.py
+++
@@ -1,9 +0,0 @@
-#### Hello world endpoint ####
-from flask_restful import Resource
-
-names = { "tom":{ "age":21, "gender":"male" }, "bill":{ "age":70, "gender":"male" } }
-
-class HelloWorld(Resource):
-    def get(self, name):
-        return names[name]
-#### End of Hello world endpoint ####
