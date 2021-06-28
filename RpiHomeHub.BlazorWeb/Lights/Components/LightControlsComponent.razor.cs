ec17edb2a414a12b1660e4fe45224cfa32521520
tommilostny <tommilostny@live.com>
2021-06-28 22:40:05 +0200

    Updated lights control component to save state, visual changes

--- RpiHomeHub.BlazorWeb/Lights/Components/LightControlsComponent.razor.cs
+++ RpiHomeHub.BlazorWeb/Lights/Components/LightControlsComponent.razor.cs
@@ -1,14 +1,21 @@
 ﻿using Microsoft.AspNetCore.Components;
 using RpiHomeHub.BlazorWeb.Lights.Services;
+using System.Threading.Tasks;
 
 namespace RpiHomeHub.BlazorWeb.Lights.Components
 {
     public partial class LightControlsComponent<TLightModel> where TLightModel : class, ILightModel, new()
     {
         [Parameter]
-        public LightServiceBase<TLightModel> Service { get; set; }
+        public LightServiceBase<TLightModel> LightService { get; set; }
 
         [Parameter]
         public TLightModel Light { get; set; }
+
+        private async Task Toggle() => Light = await LightService.ToggleAsync();
+
+        private async Task TurnOn() => Light = await LightService.TurnOnAsync();
+
+        private async Task TurnOff() => Light = await LightService.TurnOffAsync();
     }
 }
